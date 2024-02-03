d4cac7117597c2b06c7d445d0b4691bbff7c8e80
ChrisK112 <chrisgamerk@gmail.com>
2024-02-03 01:49:46 +0000

    Added TinyMCE html editor

---
+++ ProofOfConceptBLZ/Models/EmailTemplateEditModel.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ProofOfConceptBLZ.Data
+{
+    public class EmailTemplateEditModel
+    { 
+        public int Id { get; set; }
+        public string HTMLContent { get; set; }
+
+    }
+}
